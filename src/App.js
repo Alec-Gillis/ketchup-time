c45fefde622fcc5e3329db56941f90d0d51d37d1
Alec Gillis <gillisa3@msu.edu>
2020-05-17 23:26:24 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -2,21 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const app = {
+  name: 'Ketchup Time',
+  catchPhrase: 'Simple to-do list to get things done!'
+};
+
+// Fill this with all activities planned
+let toDos = [];
+
+// Defines an activity on a user's todo list
+class Activity {
+  constructor(name) {
+    this.name = name
+    this.complete = false
+  }
+}
+
+const addTodo = () => {
+  //const val = document.getElementById("toDoValue").nodeValue
+  /*document.getElementById("toDoValue").clear
+   toDos.push(new Activity(val))
+  alert(val) */
+}
+/*
+<input type="text" placeholder="What do you need todo?" id="toDoInput" value={this.state.inputValue} />
+          <button onClick={this.addTodo.bind(this)}>Add</button>*/
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          {app.name}
+        </h2>
+        {app.catchPhrase && <p>{app.catchPhrase}</p>}
+        <div>
+          PUT THE CODE ABOVE HERE ONCE IT WORKS
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.alecgillis.me"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          www.alecgillis.me
         </a>
       </header>
     </div>
